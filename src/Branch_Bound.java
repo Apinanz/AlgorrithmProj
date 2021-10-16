714d189a62f2ec7fcae645665e60a43de9bdf4bd
Natthavut Apinantakun <apinantakun_n@silpakorn.edu>
2021-10-16 20:52:35 +0700

    update

--- src/Branch_Bound.java
+++ src/Branch_Bound.java
@@ -16,19 +16,19 @@ import org.abego.treelayout.util.DefaultTreeForTreeLayout;
 import org.abego.treelayout.NodeExtentProvider;
 
 public class Branch_Bound {
-
+    
     private final int[] numbers;
     private final int target;
     private DefaultTreeForTreeLayout<Vertex> tree;
     private Font font = new Font("TH Sarabun New", Font.BOLD, 20);
     private int[] subSet;
-
+    
     public Branch_Bound(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
         this.subSet = new int[numbers.length];
         subOfSum();
-
+        
         int x = 75, y = 150; //ความห่างแต่ละ node
         DefaultConfiguration<Vertex> config = new DefaultConfiguration<>(y, x);
         NodeExtentProvider<Vertex> extent = new NodeExtentProvider<Vertex>() {
@@ -36,12 +36,12 @@ public class Branch_Bound {
             public double getWidth(Vertex v) {
                 return v.getWidth();
             }
-
+            
             @Override
             public double getHeight(Vertex v) {
                 return v.getHeight();
             }
-
+            
         };
         TreeLayout<Vertex> layout = new TreeLayout<>(tree, extent, config);
         JPanel panel = new JPanel() {
@@ -51,7 +51,7 @@ public class Branch_Bound {
                 drawEdge(g, layout.getTree().getRoot());
                 drawVertex(g);
             }
-
+            
             public void drawVertex(Graphics g) {
                 g.setFont(font);
                 for (Vertex v : layout.getNodeBounds().keySet()) {
@@ -59,7 +59,7 @@ public class Branch_Bound {
                     if (v.getPath().length() > 0) {
                         for (int i = 0; i < v.getPath().length(); i++) {
                             int currentPath = v.getPath().charAt(i) - '0';
-
+                            
                             if (subSet[i] != currentPath) {
                                 isSelect = false;
                                 break;
@@ -67,7 +67,7 @@ public class Branch_Bound {
                             isSelect = true;
                         }
                     }
-
+                    
                     Rectangle2D.Double node = layout.getNodeBounds().get(v);
                     //g.setColor(Color.WHITE);
                     g.setColor(isSelect || v.getPath().length() == 0 ? Color.GREEN : Color.white);
@@ -76,8 +76,8 @@ public class Branch_Bound {
                     g.drawRect((int) node.x, (int) node.y, (int) node.width, (int) node.height);
                     
                     String sum = v.getSum() + "";
-                    if(v.getSum()>target){
-                        sum = "-"+'\u221E';
+                    if (v.getSum() > target) {
+                        sum = "-" + '\u221E';
                     }
                     int width = getFontMetrics(font).stringWidth(sum);
                     int height = getFontMetrics(font).getHeight();
@@ -85,15 +85,15 @@ public class Branch_Bound {
                     g.drawString(sum, x - (width / 2), y + (height / 4));
                 }
             }
-
+            
             public void drawEdge(Graphics g, Vertex parent) {
-
+                
                 if (!layout.getTree().isLeaf(parent)) {
                     g.setFont(font);
                     Rectangle2D.Double node1 = layout.getNodeBounds().get(parent);
                     int x1 = (int) node1.getCenterX(), y1 = (int) node1.getCenterY();
                     for (Vertex child : layout.getTree().getChildren(parent)) {
-
+                        
                         boolean isSelect = false;
                         for (int i = 0; i < child.getPath().length(); i++) {
                             int currentPath = child.getPath().charAt(i) - '0';
@@ -136,7 +136,7 @@ public class Branch_Bound {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-
+    
     private void subOfSum() {
         Dimension dimension = size("0");
         PriorityQueue<Vertex> queue = new PriorityQueue<>(new Comparator<Vertex>() {
@@ -150,17 +150,17 @@ public class Branch_Bound {
                     return -1;
                 }
             }
-
+            
         });
         Vertex root = new Vertex(0, "", dimension.width, dimension.height);
         tree = new DefaultTreeForTreeLayout<>(root);
         queue.add(root);
-
+        
         while (!queue.isEmpty()) {
             Vertex parent = queue.poll();
             int index = parent.getPath().length();
             int sum = parent.getSum();
-
+            
             if (sum == target) {
                 String path = parent.getPath();
                 for (int i = 0; i < path.length(); i++) {
@@ -169,7 +169,7 @@ public class Branch_Bound {
                 for (int i = path.length(); i < numbers.length; i++) {
                     subSet[i] += 0;
                 }
-
+                
                 return;
             }
             for (int i = index; i < numbers.length; i++) {
@@ -186,20 +186,20 @@ public class Branch_Bound {
                     queue.add(child);
                 }
             }
-
+            
         }
-
+        
     }
-
+    
     private Dimension size(String str) {
         BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = image.createGraphics();
         int width = g.getFontMetrics(font).stringWidth(str), height = g.getFontMetrics(font).getHeight();
         return new Dimension(width, height);
     }
-
+    
     public static void main(String[] args) {
-        int[] numbers = {16,15,15,15,16,15,15};
+        int[] numbers = {16, 15, 15, 15, 16, 15, 15};
         int target = 45;
         Branch_Bound bnb = new Branch_Bound(numbers, target);
     }
